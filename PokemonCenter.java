3bb4a7afa03000f5e2c615f5c05f0590fb938416
thanaphon channarong <5910110262@email.psu.ac.th>
2020-02-16 04:20:13 +0700

    Forth commit

---
+++ PokemonCenter.java
@@ -0,0 +1,8 @@
+class PokemonCenter{
+    static String name ="kidmaiok";
+    static String managername="joy";
+
+    static void welcome(){
+        System.out.println("Wecome to Pokemoncenter");
+    }
+}
